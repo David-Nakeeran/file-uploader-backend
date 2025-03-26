25911fef00aab188fdc7168edd4eb6619cd2f999
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-26 16:50:05 +0000

    Added Post for register and login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,21 @@
 import express from "express";
+import { validateEmail, validatePassword } from "../middleware/validators.js";
+import { handleValidationErrors } from "../middleware/handleValidationErrors.js";
 import { registerPost, loginPost } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // Routes
-// Post Register new user
-router.post("/register", registerPost);
-
 // Post login user
 router.post("/login", loginPost);
+
+// Post Register new user
+router.post(
+  "/register",
+  validateEmail,
+  validatePassword,
+  handleValidationErrors,
+  registerPost
+);
+
+export default router;
