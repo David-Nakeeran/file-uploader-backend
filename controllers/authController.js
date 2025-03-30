3174cc543302568fedc8fe0fd46cd948051ab875
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-30 19:18:47 +0100

    Added call to createUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,22 @@
 import asyncHandler from "express-async-handler";
+import CustomError from "../errors/customError.js";
+import { createUser } from "../services/userService.js";
+import bcrypt from "bcryptjs";
 
 export const registerPost = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
   //check if user exists
 
   // if user exists throw custom error 400
 
   //has password before saving
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
   //save user in database
+  const newUser = await createUser(req.body, hashedPassword);
+
+  if (!newUser) {
+    throw new CustomError(500, "Internal server error");
+  }
 
   res
     .status(201)
