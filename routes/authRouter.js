cb394cc917b0d70e6c346c21c79a4ae6b4439371
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-30 19:15:31 +0100

    formatting

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { validateEmail, validatePassword } from "../middleware/validators.js";
-import { handleValidationErrors } from "../middleware/handleValidationErrors.js";
-import { registerPost, loginPost } from "../controllers/authController.js";
+import handleValidationErrors from "../middleware/handleValidationErrors.js";
+import { registerPost } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // Routes
 // Post login user
-router.post("/login", loginPost);
+// router.post("/login", loginPost);
 
 // Post Register new user
 router.post(
-  "/register",
+  "/",
   validateEmail,
   validatePassword,
   handleValidationErrors,
