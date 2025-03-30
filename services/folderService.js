e87a4d6df6699cfc0ad85722e8cad0030fba1035
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-30 19:16:16 +0100

    Added createFolder

--- services/folderService.js
+++ services/folderService.js
@@ -1,4 +1,26 @@
-export const isFolderPathUnique = async (newPath) => {
-  // get all folder from db
-  // check collection if it the new path does not exist
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+export const allFolders = async () => {
+  const folders = await prisma.folder.findMany();
+  return folders;
 };
+
+export const createFolder = async (newFolder) => {
+  const { name, path, external_id } = newFolder;
+  const folder = await prisma.folder.create({
+    data: {
+      folderName: name,
+      folderPath: path,
+      externalId: external_id,
+    },
+  });
+  return folder;
+};
+
+// export const isFolderPathUnique = async (newPath) => {
+// get all folder from db
+// check collection if it the new path does not exist
+// };
