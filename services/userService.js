696a478be7e16d94beab955d2005fb3fa6271eb3
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-31 21:10:29 +0100

    Updated updateUserVerified

--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,8 @@
 import CustomError from "../errors/customError.js";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { generateEmailVerificationToken } from "../auth/emailAuth.js";
+import { sendVerificationEmail } from "../services/mailService.js";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
@@ -14,12 +16,39 @@ export const createUser = async (newUser, password) => {
         password: password,
       },
     });
+
+    const verificationToken = generateEmailVerificationToken(user.id);
+
+    const verificationEmail = await sendVerificationEmail(
+      user.email,
+      verificationToken
+    );
+
     return user;
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       if (err.code === "P2002") {
         throw new CustomError(400, "Email is already in use");
       }
+    }
+  }
+};
+
+export const updateUserVerified = async (userId) => {
+  try {
+    const userVerified = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        isVerified: true,
+        emailVerificationToken: null,
+      },
+    });
+    return userVerified;
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(err);
     }
   }
 };
