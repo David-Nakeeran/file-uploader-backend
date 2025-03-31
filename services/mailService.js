d9374696b9be11732be6972847cbe0bcf7eaa027
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-31 21:05:18 +0100

    Added sendVerificationEmail

---
+++ services/mailService.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Resend } from "resend";
+import CustomError from "../errors/customError.js";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (to, verificationToken) => {
+  try {
+    const verificationLink = `${process.env.BASE_URL}/verify-email`;
+    const { data, error } = await resend.emails.send({
+      from: "NoReply <noreply@davidnakeeran.dev>",
+      to: [to],
+      subject: "Verify Your Account",
+      html: `<strong>Click the link to verify your mail:</strong><br>
+      <form action="${verificationLink}" method="POST">
+      <input type="hidden" name="token" value="${verificationToken}">
+      <button type="submit">Verify Email</button>
+      </form>`,
+    });
+    return data;
+  } catch (error) {
+    throw new CustomError(500, "Email could not be sent");
+  }
+};
