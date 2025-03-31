c668ad94a0f29a67ab5678d7c07ac727f5262403
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-31 21:09:17 +0100

    Updated registerPost

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 import asyncHandler from "express-async-handler";
 import CustomError from "../errors/customError.js";
-import { createUser } from "../services/userService.js";
+import { createUser, updateUserVerified } from "../services/userService.js";
 import bcrypt from "bcryptjs";
+import { verifyEmailVerificationToken } from "../auth/emailAuth.js";
 
 export const registerPost = asyncHandler(async (req, res, next) => {
   //check if user exists
@@ -21,4 +22,28 @@ export const registerPost = asyncHandler(async (req, res, next) => {
   res
     .status(201)
     .json({ success: true, message: "User registered successfully" });
+});
+
+export const verifyEmail = asyncHandler(async (req, res, next) => {
+  const { token } = req.query;
+
+  if (!token) {
+    throw new CustomError(400, "Invalid or missing token");
+  }
+
+  const { userId } = verifyEmailVerificationToken(token);
+  if (!userId) {
+    throw new CustomError(400, "Invalid or expired token");
+  }
+
+  // update user in database and change isVerified === true
+  const userUpdated = updateUserVerified(userId);
+
+  if (!userUpdated) {
+    throw new CustomError(500, "Something went wrong");
+  }
+
+  res
+    .status(200)
+    .json({ success: true, message: "Email successfully verified" });
 });
