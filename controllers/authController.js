1b413d459e8d4371ac7f3ce63d3d8ca02305585d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-01 20:14:13 +0100

    Updated verifyEmail

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import CustomError from "../errors/customError.js";
-import { createUser, updateUserVerified } from "../services/userService.js";
+import {
+  createUser,
+  updateUserVerified,
+  getUserById,
+} from "../services/userService.js";
 import bcrypt from "bcryptjs";
 import { verifyEmailVerificationToken } from "../auth/emailAuth.js";
 
@@ -32,18 +36,28 @@ export const verifyEmail = asyncHandler(async (req, res, next) => {
   }
 
   const { userId } = verifyEmailVerificationToken(token);
-  if (!userId) {
+
+  const user = await getUserById(userId);
+
+  if (!user.emailVerificationToken) {
+    throw new CustomError(400, "This email has already been verified");
+  }
+
+  const isTokenValid = await bcrypt.compare(token, user.emailVerificationToken);
+
+  if (!isTokenValid) {
     throw new CustomError(400, "Invalid or expired token");
   }
 
-  // update user in database and change isVerified === true
-  const userUpdated = updateUserVerified(userId);
+  const userUpdated = await updateUserVerified(userId);
 
   if (!userUpdated) {
-    throw new CustomError(500, "Something went wrong");
+    throw new CustomError(500, "Internal server error");
   }
 
-  res
-    .status(200)
-    .json({ success: true, message: "Email successfully verified" });
+  res.status(302).redirect("api/auth/login?verified=true");
 });
+
+export const requestNewVerificationEmail = asyncHandler(
+  async (req, res, next) => {}
+);
