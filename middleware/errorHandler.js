2251a3d3cda12e84c0230fa00cb8419271a7e5e0
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-01 20:15:23 +0100

    Added redirect

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,10 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
+  if (err.redirectTo) {
+    return res.redirect(err.redirectTo);
+  }
+
   res.status(statusCode).json({
     success: false,
     error: {
