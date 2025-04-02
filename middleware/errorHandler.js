f9f77bdf9f4693f71ca4fafd8ef51b8817144cb9
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-02 21:15:21 +0100

    Updated error handler incorp prisma errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,26 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  // Handle Prisma errors
+  switch (err.code) {
+    case "P2002":
+      statusCode = 409;
+      message = `${err.meta?.target?.join(", ")} already registered`;
+      break;
+    case "P2025":
+    case "P2001":
+      statusCode = 404;
+      message = "Record not found";
+      break;
+    case "P2007":
+      statusCode = 400;
+      message = "Validation error: Invalid data provided";
+      break;
+    default:
+      statusCode = 500;
+      message = "Unknown database error occurred";
+  }
 
   if (err.redirectTo) {
     return res.redirect(err.redirectTo);
