cec1ab807cddfbf9fd7be82b7e3b0d25532cc1df
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-06 16:38:05 +0100

    Updated token funcs

--- auth/auth.js
+++ auth/auth.js
@@ -3,15 +3,20 @@ import CustomError from "../errors/customError.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const generateToken = (user) => {
+// export const generateToken = (user, secret, expires) => {
+//   const payload = { userId: user.id };
+//   return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "5m" });
+// };
+
+export const generateToken = (user, secret, expires) => {
   const payload = { userId: user.id };
-  return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "5m" });
+  return jwt.sign(payload, secret, { expiresIn: expires });
 };
 
 export const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  console.log(req.headers);
-  const token = authHeader && authHeader.split(" ")[1];
+  // Extract access token from the HttpOnly cookie
+  console.log(req.cookies);
+  const token = req.cookies.accessToken;
 
   if (!token) {
     throw new CustomError(401, "Access denied, no token provided");
@@ -22,7 +27,25 @@ export const authenticateToken = (req, res, next) => {
     req.user = decoded.userId;
     next();
   } catch (err) {
-    throw new CustomError(403, "Invalid or expired token");
+    throw new CustomError(401, "Invalid or expired token");
+    // if token expires, frontend re-directs to login
+  }
+};
+
+export const authenticateRefreshToken = (req, res, next) => {
+  // Extract access token from the HttpOnly cookie
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    throw new CustomError(401, "No refresh token provided");
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (err) {
+    throw new CustomError(401, "Invalid or expired token");
     // if token expires, frontend re-directs to login
   }
 };
