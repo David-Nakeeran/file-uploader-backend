acfd08f198c1971780760707b444a2cd984bd23a
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-07 16:11:26 +0100

    Mode switch into if

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,23 +3,25 @@ const errorHandler = (err, req, res, next) => {
   let message = err.message || "Internal Server Error";
 
   // Handle Prisma errors
-  switch (err.code) {
-    case "P2002":
-      statusCode = 409;
-      message = `${err.meta?.target?.join(", ")} already registered`;
-      break;
-    case "P2025":
-    case "P2001":
-      statusCode = 404;
-      message = "Record not found";
-      break;
-    case "P2007":
-      statusCode = 400;
-      message = "Validation error: Invalid data provided";
-      break;
-    default:
-      statusCode = 500;
-      message = "Unknown database error occurred";
+  if (err.code) {
+    switch (err.code) {
+      case "P2002":
+        statusCode = 409;
+        message = `${err.meta?.target?.join(", ")} already registered`;
+        break;
+      case "P2025":
+      case "P2001":
+        statusCode = 404;
+        message = "Record not found";
+        break;
+      case "P2007":
+        statusCode = 400;
+        message = "Validation error: Invalid data provided";
+        break;
+      default:
+        statusCode = 500;
+        message = "Unknown database error occurred";
+    }
   }
 
   if (err.redirectTo) {
