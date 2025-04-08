1de8aec102801e8ae3e43606a3d2bc575b57c139
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-08 16:05:47 +0100

    auth to folderGetAll route

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,22 +1,30 @@
 import express from "express";
 import {
-  folderGet,
+  folderGetAll,
   // folderGetById,
   folderPost,
   folderPut,
   folderDelete,
 } from "../controllers/folderController.js";
 import { authenticateToken } from "../auth/auth.js";
+import { validateFolderName } from "../middleware/validators.js";
+import handleValidationErrors from "../middleware/handleValidationErrors.js";
 
 const router = express.Router();
 
 // Routes
-router.get("/", folderGet);
+router.get("/", authenticateToken, folderGetAll);
 // router.get("/:id", folderGetById);
 router.put("/:id", folderPut);
 
 // Create folder
-router.post("/", authenticateToken, folderPost);
+router.post(
+  "/",
+  authenticateToken,
+  validateFolderName,
+  handleValidationErrors,
+  folderPost
+);
 
 router.delete("/", folderDelete);
 
