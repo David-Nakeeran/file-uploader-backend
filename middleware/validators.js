00419731354e7be91e136e23462330c8a2a7cee8
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-08 16:07:05 +0100

    Added validateFolderName

--- middleware/validators.js
+++ middleware/validators.js
@@ -22,3 +22,18 @@ export const validatePassword = [
     .notEmpty()
     .withMessage("Password cannot be empty"),
 ];
+
+export const validateFolderName = [
+  body("folderName")
+    .trim()
+    .notEmpty()
+    .withMessage("Folder name cannot be empty")
+    .isLength({ min: 1, max: 255 })
+    .withMessage(
+      "Folder name must be at least 1 characters long and 255 characters long"
+    )
+    .matches(/^(?!v\d)[a-zA-Z][a-zA-Z0-9-]*$/)
+    .withMessage(
+      "Folder name must start with a letter and can only contain letters, numbers, hyphens, and underscores. Cannot start with 'v' followed by a number"
+    ),
+];
