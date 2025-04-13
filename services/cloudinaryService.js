9033eb00ef8483ef52eba6c1b46d74838d2ba944
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-13 16:03:52 +0100

    Added cloudinary service

---
+++ services/cloudinaryService.js
@@ -0,0 +1,18 @@
+import cloudinary from "../utils/cloudinaryConfig.js";
+import CloudinaryError from "../errors/cloudinaryError.js";
+
+export const cloudinaryUpdateFolderName = async (
+  currentFolderPath,
+  folderName
+) => {
+  try {
+    return await cloudinary.api.rename_folder(
+      `${currentFolderPath}`,
+      `${folderName}`
+    );
+  } catch (error) {
+    console.log(`Message: ${Object.keys(error)}`);
+    console.log(error.error);
+    throw new CloudinaryError(409, "A folder with that name already exists");
+  }
+};
