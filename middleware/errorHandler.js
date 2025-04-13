c1d3557f559a6614b3fe7c506a02011bf16299b7
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-13 16:04:16 +0100

    formatting

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err.error);
+  // console.log(err.error);
 
   let statusCode = err.statusCode || 500;
   let message = err.message || "Internal Server Error";
