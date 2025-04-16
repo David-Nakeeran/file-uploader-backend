7d0782fae76b205f022d774df17ed03af2e6d10e
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-16 20:51:20 +0100

    Added uploads route to app.use

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 import folderRoutes from "./routes/folderRoutes.js";
 import authRouter from "./routes/authRouter.js";
+import fileRoutes from "./routes/fileRoutes.js";
 import { deleteExpiredUser } from "./services/userService.js";
 import passport from "./auth/passportConfig.js";
 import cookieParser from "cookie-parser";
+import { fileUpload } from "./controllers/fileController.js";
 
 const port = process.env.PORT || 8000;
 
@@ -28,6 +30,7 @@ app.use(cookieParser());
 // Public
 app.use("/api/auth", authRouter);
 app.use("/api/folders", folderRoutes);
+app.use("/api/uploads", fileRoutes);
 
 // Protected
 // app.use("/files", filesRouter);
