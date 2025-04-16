07178166c17a8239a92ed2641abcc82f9c8a94c5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-16 20:52:39 +0100

    Added uploads route

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { upload } from "../middleware/multer.js";
+import { fileUpload } from "../controllers/fileController.js";
+import { authenticateToken } from "../auth/auth.js";
 
 const router = express.Router();
 
 // Routes
-router.post("/upload");
+router.post("/", authenticateToken, upload.single("file"), fileUpload);
+
+export default router;
