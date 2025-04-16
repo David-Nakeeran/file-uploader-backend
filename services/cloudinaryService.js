1d855bf34c6b94d8bf052b4ba9becb1697900048
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-16 20:53:39 +0100

    Added streamUpload

--- services/cloudinaryService.js
+++ services/cloudinaryService.js
@@ -35,3 +35,22 @@ export const cloudinaryDeleteFolder = async (folderPath) => {
     // Chore: find out error
   }
 };
+
+export const streamUpload = (fileBuffer, folder, publicId) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto",
+        folder: folder,
+        public_id: publicId,
+      },
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(result);
+      }
+    );
+    stream.end(fileBuffer);
+  });
+};
