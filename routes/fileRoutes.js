d30e2772a5e87f17ce256d6fa933501d1df7513c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-19 14:56:30 +0100

    Added move file route

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { upload } from "../middleware/multer.js";
-import { fileUpload } from "../controllers/fileController.js";
+import { fileUpload, fileMove } from "../controllers/fileController.js";
 import { authenticateToken } from "../auth/auth.js";
 
 const router = express.Router();
 
 // Routes
 router.post("/", authenticateToken, upload.single("file"), fileUpload);
+
+router.post("/:id", authenticateToken, fileMove);
 
 export default router;
