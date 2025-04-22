846f095ad46d73ddb06caa47c4ac9d90e4aeb8c2
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-22 16:25:44 +0100

    Removed route file

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 import folderRoutes from "./routes/folderRoutes.js";
 import authRouter from "./routes/authRouter.js";
-import fileRoutes from "./routes/fileRoutes.js";
 import { deleteExpiredUser } from "./services/userService.js";
 import passport from "./auth/passportConfig.js";
 import cookieParser from "cookie-parser";
-import { fileUpload } from "./controllers/fileController.js";
 
 const port = process.env.PORT || 8000;
 
@@ -30,10 +28,6 @@ app.use(cookieParser());
 // Public
 app.use("/api/auth", authRouter);
 app.use("/api/folders", folderRoutes);
-app.use("/api/uploads", fileRoutes);
-
-// Protected
-// app.use("/files", filesRouter);
 
 // Error
 app.use(errorHandler);
