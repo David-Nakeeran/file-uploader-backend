bf8977bdd63fc621bbefef2782952ac799eb0530
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-04-23 15:58:13 +0100

    Updated email message

--- services/mailService.js
+++ services/mailService.js
@@ -8,13 +8,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (to, verificationToken) => {
   try {
     const verificationLink = `${process.env.BASE_URL}/verify-email?token=${verificationToken}`;
-    const { data, error } = await resend.emails.send({
+    const { data } = await resend.emails.send({
       from: "NoReply <noreply@davidnakeeran.dev>",
       to: [to],
       subject: "Verify Your Account",
-      html: `<strong>Click the link to verify your mail:</strong><br><a href="${verificationLink}">Verify email</a>
-      `,
+      html: `
+      <p>Thank you for signing up. Please confirm your email address by clicking the link below.</p>
+      <strong>Click the link to verify your mail:</strong><br><a href="${verificationLink}">Verify email</a>
+      <p>If you didn't register for an account, please ignore this email.</p>`,
     });
+
     return data;
   } catch (error) {
     throw new CustomError(500, "Email could not be sent");
